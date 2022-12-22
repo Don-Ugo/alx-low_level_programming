465258364a8c54208cb323fff13e25a8ef8ec8a7
Don-Ugo <ugochukwualex87@yahoo.com>
2022-12-21 23:59:38 -0800

    Pointers are arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * print_array- Prints an array of numbers with n member
+ *
+ * @a: array to be printed
+ *
+ * @n: number of items in the array
+ *
+ */
+
+void print_array(int *a, int n)
+
+{
+
+int i;
+for (i = 0; i < n; i++)
+{
+if (i == n - 1)
+{
+printf("%d", a[i]);
+}
+else
+{
+printf("%d", a[i]);
+
+printf("%s", ", ");
+}
+
+}
+
+printf("%c", '\n');
+
+}
