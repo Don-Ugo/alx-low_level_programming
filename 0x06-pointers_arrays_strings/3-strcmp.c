4499f64ee5917760429ef2b3ba673823584eda53
Don-Ugo <ugochukwualex87@yahoo.com>
2022-12-23 00:35:33 -0800

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,23 @@
 int _strcmp(char *s1, char *s2)
 {
 int x;
+
 x = 0;
-while (s1[x] != '\0' && s2[x] != '\0')
+
+while (*s1 != '\0' && *s2 != '\0')
 {
-if (s1[x] != s2[x])
+if (*s1 > *s2)
+return (1);
+else if (*s1 < *s2)
+return (-1);
+s1++;
+s2++;
+x++;
 }
-{
-return (s1[x] - s2[x]);
-}
-i++;
-}
+if (s1[x] == '\0' && s2[x] != '\0')
+return (-1);
+else if (s2[x] == '\0' && s1[x] != '\0')
+return (1);
+else
 return (0);
-
-
+}
